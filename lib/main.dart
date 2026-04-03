a3d28afe6217d2acbfcfc10d3cf3503c6a50a46f
Daniel Michele <daniel@michele-it.de>
2026-04-03 21:17:12 +0200

    Fix suggestions and failing test, create a wf to add it as PR dependency

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ import 'screens/home_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final state = AppState();
-  await Future.wait([
-    state.statsService.load(),
-    state.loadSettings(),
-  ]);
+  await Future.wait([state.statsService.load(), state.loadSettings()]);
   runApp(TeamUpApp(state: state));
 }
 
@@ -21,12 +18,6 @@ class TeamUpApp extends StatefulWidget {
 }
 
 class _TeamUpAppState extends State<TeamUpApp> {
-  @override
-  void dispose() {
-    widget.state.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
